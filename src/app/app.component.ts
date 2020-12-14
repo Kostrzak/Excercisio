de80d35a1c03d8900cdaf7bbd730c46154145448
Kostrzak <kostrzak18@gmail.com>
2020-12-14 02:12:39 +0100

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
-  exc : ExcerciseReadCard = {
-    imgSrc : "https://i.ytimg.com/vi/i6wTUg_tA-A/maxresdefault.jpg",
-    name : "Face Pullss"
-  }
+export class AppComponent {
+  name = "Angular";
+  exc: ExcerciseReadCard = {
+    //imgSrc: "https://i.imgur.com/eWhTVg4.png",
+    imgSrc: "https://i.ytimg.com/vi/i6wTUg_tA-A/maxresdefault.jpg",
+    name: "Face Pullss"
+  };
 }
 
 export class ExcerciseReadCard {
