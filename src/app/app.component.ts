b994a89a3eed44ef042c97a5dda1bd3283cc7de2
kostrzak <kostrzak18@gmail.com>
2021-12-26 00:10:14 +0100

    Angular app with AngularFire created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface Item {
+  name: string,
+};
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ExcercisioTotalNew';
+ // item$: Observable<Item[]>;
+  itemki$: Observable<any[]>;
+  constructor(firestore: Firestore) {
+    const collectio = collection(firestore, 'Excercises');
+    this.itemki$ = collectionData(collectio);
+ //   this.itemki$ = collection(firestore,'Excercises');
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'habitato',
+    appId: '1:16802022201:web:2a2e99d07a08d0aea55ee8',
+    databaseURL: 'https://habitato.firebaseio.com',
+    storageBucket: 'habitato.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyC3WX1eC-OooBfYZJwU2AIUQe-btrt2JlA',
+    authDomain: 'habitato.firebaseapp.com',
+    messagingSenderId: '16802022201',
+  },
   production: true
 };
